05ab0bf966362596cbcaceca83b98d5e0a27d16b
Fahmy rosyadi <fahmyrosyadi29@gmail.com>
2025-10-16 23:07:23 +0700

    feat(ui): improve search UX with loading state and enhance responsiveness

--- src/app/features/products/product-list/product-list.ts
+++ src/app/features/products/product-list/product-list.ts
@@ -4,20 +4,26 @@ import { Product } from '../../../shared/models/product.model';
 import { ApiService } from '../../../core/services/api.service';
 import { ProductCard } from '../../../shared/components/product-card/product-card';
 import { combineLatest, map, Observable } from 'rxjs';
+import { Spinner } from '../../../shared/components/spinner/spinner';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, ProductCard],
+  imports: [CommonModule, ProductCard, Spinner],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss'
 })
 export class ProductList implements OnInit{
     products$!: Observable<Product[]>;
+    isLoading = true;
+
     constructor(private apiServices : ApiService) {};
 
     ngOnInit(): void {
-      const allProducts$ = this.apiServices.getProducts();
+      const allProducts$ = this.apiServices.getProducts().pipe(
+        finalize(() => this.isLoading = false)
+      );
       const searchTerm$ = this.apiServices.searchTerm$;
       this.products$ = combineLatest([allProducts$, searchTerm$]).pipe(
       map(([products, term]) => 
