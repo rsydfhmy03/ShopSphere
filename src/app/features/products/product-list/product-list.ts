cf9f3b0a9a054f39cd5aa7d00f96f57b7d8ecffe
Fahmy rosyadi <fahmyrosyadi29@gmail.com>
2025-10-16 22:37:14 +0700

    feat(products): implement product card and display products in responsive grid

--- src/app/features/products/product-list/product-list.ts
+++ src/app/features/products/product-list/product-list.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
 import { ApiService } from '../../../core/services/api.service';
+import { ProductCard } from '../../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss'
 })
-export class ProductList {
+export class ProductList implements OnInit{
     products : Product[] = [];
 
     constructor(private apiServices : ApiService) {};
--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component , Input } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss'
 })
 export class ProductCard {
-
+  @Input() product!: Product;
 }
