4c5da18b0949030f7443106e46c3ca03d216d495
Fahmy rosyadi <fahmyrosyadi29@gmail.com>
2025-10-17 00:54:31 +0700

    feat(cart): build cart view page with item management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './features/products/product-list/product-list'
 import { ProductDetail } from './features/products/product-detail/product-detail';
+import { CartView } from './features/cart/cart-view/cart-view';
 
 export const routes: Routes = [
   { path: '', component: ProductList },
-  {path: 'product/:id', component: ProductDetail}
+  {path: 'product/:id', component: ProductDetail},
+  {path: 'cart', component : CartView}
 ];
--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs'; 
 import { Product } from '../../shared/models/product.model';
 
 export interface CartItem {
@@ -14,7 +14,13 @@ export class CartService {
   private cartItemsSubject = new BehaviorSubject<CartItem[]>([]);
   public cartItems$ = this.cartItemsSubject.asObservable();
 
-  constructor() { }
+  public totalAmount$: Observable<number>;
+
+  constructor() {
+    this.totalAmount$ = this.cartItems$.pipe(
+      map(items => items.reduce((total, item) => total + (item.product.price * item.quantity), 0))
+    );
+  }
 
   addToCart(product: Product): void {
     const currentItems = this.cartItemsSubject.getValue();
@@ -27,6 +33,25 @@ export class CartService {
     }
 
     this.cartItemsSubject.next([...currentItems]);
-    alert(`${product.title} has been added to your cart!`);
+  }
+
+  removeFromCart(productId: number): void {
+    const currentItems = this.cartItemsSubject.getValue();
+    const updatedItems = currentItems.filter(item => item.product.id !== productId);
+    this.cartItemsSubject.next(updatedItems);
+  }
+
+  updateQuantity(productId: number, change: number): void {
+    const currentItems = this.cartItemsSubject.getValue();
+    const itemToUpdate = currentItems.find(item => item.product.id === productId);
+
+    if (itemToUpdate) {
+      itemToUpdate.quantity += change;
+      if (itemToUpdate.quantity <= 0) {
+        this.removeFromCart(productId);
+      } else {
+        this.cartItemsSubject.next([...currentItems]);
+      }
+    }
   }
 }
